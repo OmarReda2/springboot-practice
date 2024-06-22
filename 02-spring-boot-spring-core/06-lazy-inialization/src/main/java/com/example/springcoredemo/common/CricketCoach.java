125cc14d2765bf27431dd3da69bb558de7197ddf
Omar Reda <omarredaelsayied@gmail.com>
2024-06-22 23:13:45 +0300

    lazy initialization Coding P1

--- 02-spring-boot-spring-core/06-lazy-inialization/src/main/java/com/example/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/06-lazy-inialization/src/main/java/com/example/springcoredemo/common/CricketCoach.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class CricketCoach implements Coach{
+
+    public CricketCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkOut() {
--- 02-spring-boot-spring-core/06-lazy-inialization/src/main/java/com/example/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/06-lazy-inialization/src/main/java/com/example/springcoredemo/rest/DemoController.java
@@ -12,8 +12,9 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public  DemoController(Coach theCoach){
-       myCoach = theCoach;
+    public  DemoController(@Qualifier("cricketCoach") Coach theCoach){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
     }
 
     @GetMapping("/dailyworkout")

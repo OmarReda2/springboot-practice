088a635fd657a8f1ed5b1258a6b3ca79da699b5b
Omar Reda <omarredaelsayied@gmail.com>
2024-06-24 02:21:49 +0300

    bean lifecycle methods - @PreDestroy & @ PostConstruct

--- 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/example/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/example/springcoredemo/common/CricketCoach.java
@@ -1,4 +1,6 @@
 package com.example.springcoredemo.common;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,6 +9,19 @@ public class CricketCoach implements Coach{
     public CricketCoach(){
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
+
+    // define our init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
+
 
     @Override
     public String getDailyWorkOut() {

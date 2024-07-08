77e9b55cfd15e66751cfd5ce237937771bf986ec
Omar Reda <omarredaelsayied@gmail.com>
2024-07-08 20:40:35 +0300

    Spring Boot Rest Controller

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/myapp/demo/rest/DemoRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/myapp/demo/rest/DemoRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoRestController  {
     // add code for the "hello" endpoint
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String sayHello(){
         return "Hello World!";
     }

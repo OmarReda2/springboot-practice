de42d845c36f7828f52d40234aff96d5215fe0d8
Omar Reda <omarredaelsayied@gmail.com>
2024-07-09 14:36:38 +0300

    Spring Boot DAO Add, Update, Delete - Creating Service

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/myapp/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/myapp/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -4,12 +4,13 @@ import com.myapp.springboot.cruddemo.dao.EmployeeDAO;
 import com.myapp.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
-    
+
     private EmployeeDAO employeeDAO;
     
     @Autowired
@@ -20,5 +21,22 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }

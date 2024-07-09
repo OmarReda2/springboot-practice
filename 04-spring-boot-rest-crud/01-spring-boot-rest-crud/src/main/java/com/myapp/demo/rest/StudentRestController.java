2762749cf1ec6f48e9fe477d1c600e5ed5f67124
Omar Reda <omarredaelsayied@gmail.com>
2024-07-09 08:06:35 +0300

    Spring Boot REST Exception Handling - Add another @ExcptionHandler for BAD REQUEST

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/myapp/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/myapp/demo/rest/StudentRestController.java
@@ -65,4 +65,19 @@ public class StudentRestController {
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+
+        // create a StudentErrorResponse
+
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
 }

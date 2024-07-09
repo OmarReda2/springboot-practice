9d8cd225599456f0a8b53f84164a05ecaab3534a
Omar Reda <omarredaelsayied@gmail.com>
2024-07-09 14:28:43 +0300

    Spring Boot DAO Add, Update, Delete - Creating DAO

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/myapp/springboot/cruddemo/dao/EmloyeeDAOJpaImpl.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/myapp/springboot/cruddemo/dao/EmloyeeDAOJpaImpl.java
@@ -26,10 +26,41 @@ public class EmloyeeDAOJpaImpl implements EmployeeDAO{
         // create query
         TypedQuery<Employee> theQuery = entityManager.createQuery("FROM Employee", Employee.class);
 
-        // excutquery and get result list
+        // excute query and get result list
         List<Employee> employees = theQuery.getResultList();
 
         // return the results
         return employees;
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        // get Employee
+
+        // return theEmployee
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+
+        // save employee
+        Employee dbEmployee = entityManager.merge(theEmployee);
+
+        // return the dbEmployee
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
+        // find employee by id
+        Employee thEmployee = entityManager.find(Employee.class, theId);
+
+        // remove employee
+        entityManager.remove(thEmployee);
+
+
     }
 }

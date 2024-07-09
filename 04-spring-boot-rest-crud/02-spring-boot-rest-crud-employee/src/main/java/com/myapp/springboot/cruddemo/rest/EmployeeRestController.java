9fe195edd3cd9801eddc9d213ce1399682c62946
Omar Reda <omarredaelsayied@gmail.com>
2024-07-09 19:11:59 +0300

    Spring Boot REST Get Single Employee

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/myapp/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/myapp/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,8 +3,11 @@ package com.myapp.springboot.cruddemo.rest;
 import com.myapp.springboot.cruddemo.dao.EmployeeDAO;
 import com.myapp.springboot.cruddemo.entity.Employee;
 import com.myapp.springboot.cruddemo.service.EmployeeService;
+import com.myapp.springboot.cruddemo.service.EmployeeServiceImpl;
 import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +21,7 @@ public class EmployeeRestController {
 
 
 
+    @Autowired
     public EmployeeRestController(EmployeeService theEmployeeService){
         employeeService = theEmployeeService;
     }
@@ -27,5 +31,17 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return  theEmployee;
     }
 }

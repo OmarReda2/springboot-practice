7c96d10745b6053063da27920ee0e5de56f3345b
Omar Reda <omarredaelsayied@gmail.com>
2024-07-10 17:51:41 +0300

    Spring Boot REST Spring Data REST - customize endpoint P2

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/myapp/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/myapp/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.myapp.springboot.cruddemo.dao;
 
 import com.myapp.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // no need to write code

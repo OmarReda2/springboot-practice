193ddc811d114645e1ac9d1bf723bcb44a0388d8
Omar Reda <omarredaelsayied@gmail.com>
2024-09-07 18:08:44 +0300

    spring mvc intro, security & crud mvc operation

--- 06-spring-boot-mvc/01-thymeleafdemo-helloworld/src/main/java/com/myapp/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-mvc/01-thymeleafdemo-helloworld/src/main/java/com/myapp/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,18 +1,50 @@
 package com.myapp.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping("/showform")
+    @GetMapping("/showform")
     public String showForm(){
         return "helloworld-form";
     }
 
     @RequestMapping("/processForm")
     public String processForm(){
+        return "helloworld";
+    }
+
+    @GetMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+
+        String theName = request.getParameter("studentName");
+
+        theName = theName.toUpperCase();
+
+        String result = "Yo " + theName;
+
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+
+    @PostMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+
+        theName = theName.toUpperCase();
+
+        String result = "Hey My Friend from V3 " + theName;
+
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 }
